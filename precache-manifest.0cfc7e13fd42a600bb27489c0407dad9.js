934bc0f6fc9641dad26bd016b0e6a27ae4d1abee
Nikita Nagalsky <nikita@ahub.com>
2020-06-19 14:54:53 +0300

    Updates

---
+++ precache-manifest.0cfc7e13fd42a600bb27489c0407dad9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed24f63ffd487c7bef5c5190badc02c3",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "c84acd73e77911d1101e",
+    "url": "/react-spa/static/css/main.6cbafd89.chunk.css"
+  },
+  {
+    "revision": "f9fcd905341ae5915aeb",
+    "url": "/react-spa/static/js/2.a751721c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spa/static/js/2.a751721c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84acd73e77911d1101e",
+    "url": "/react-spa/static/js/main.0400100c.chunk.js"
+  },
+  {
+    "revision": "6f4e8cb785fc783596d2",
+    "url": "/react-spa/static/js/runtime-main.435fab0e.js"
+  },
+  {
+    "revision": "325af4b117302189d7b4248bb9a072f7",
+    "url": "/react-spa/static/media/MuseoModerno-Bold.325af4b1.woff"
+  },
+  {
+    "revision": "b11824a9a4b8422580cc5c75798c20db",
+    "url": "/react-spa/static/media/MuseoModerno-Bold.b11824a9.woff2"
+  },
+  {
+    "revision": "86494c8935a6f237f7591db5148dcc2f",
+    "url": "/react-spa/static/media/MuseoModerno-Light.86494c89.woff"
+  },
+  {
+    "revision": "8a3029d45169aa1981ed1ae70dcc2374",
+    "url": "/react-spa/static/media/MuseoModerno-Light.8a3029d4.woff2"
+  },
+  {
+    "revision": "5584b92c948a63ac068aa0f3a2de3a3e",
+    "url": "/react-spa/static/media/MuseoModerno-Regular.5584b92c.woff2"
+  },
+  {
+    "revision": "f338b4234f3981a9b66e49612dacbbb8",
+    "url": "/react-spa/static/media/MuseoModerno-Regular.f338b423.woff"
+  }
+]);
